902c2454a1936729ea9268697f511f932241e07b
Elissandro Vieira <115590740+elissandrovieira@users.noreply.github.com>
2022-10-27 01:42:20 -0300

    First submit.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let menuMobile = document.querySelector('.menuMobile')
+let hamburger = document.querySelector('.hamburger')
+
+let menuOpen = false
+
+function activeMenu(){
+    if(menuOpen === false){
+        menuMobile.classList.add('open')
+        hamburger.classList.add('active')
+        menuOpen = true
+    }else{
+        menuMobile.classList.remove('open')
+        hamburger.classList.remove('active')
+        menuOpen = false
+    }
+}
